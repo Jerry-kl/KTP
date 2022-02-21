a3908daceec5cf7d68adf27dfbb1b87f17d17cc2
Jerry-kl <79540831+Jerry-kl@users.noreply.github.com>
2022-02-21 15:13:01 +0300

    KTP Labs

---
+++ Labs_klimushin_BST2003_KTP-master/lr1_1/src/Primes.java
@@ -0,0 +1,26 @@
+package src;
+
+public class Primes 
+{    public static void main(String[] args) 
+    {
+        for (int i = 2; i < 100; i++) 
+        {
+            if (isPrime(i)) 
+            {
+                System.out.println(i);
+            }
+        }
+    }
+
+
+    public static boolean isPrime(int n) 
+    {
+        for(int i = 2; i < n; i++)
+        {
+            
+            if (n % i == 0) //если остаток числа деления на 2 = 0, то не простое
+                return false;  // Проверка простоты числа
+        }
+        return true;
+    } 
+}
---
+++ Labs_klimushin_BST2003_KTP-master/lr1_2/src/Palindrome.java
@@ -0,0 +1,30 @@
+    public class Palindrome 
+{
+    public static void main(String[] args) {
+        String[] arr = {"java", "Palindrome", "madam", "racecar", "apple", "kayak", "song", "noon"};
+        for (int i = 0; i < arr.length; i++) 
+        {
+            String s = arr[i];
+            if (isPalindrome(s))
+                System.out.printf("%s это палиндром\n", s);
+            else
+                System.out.printf("%s это не палиндром\n", s);
+        }
+    }
+    
+    public static String reverse(String s) {
+        // Переворачивает строку в обратном порядке
+        String result = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            result += s.charAt(i); // достает букву по индексу из строки
+        }
+        return result;
+    }
+    
+    public static boolean isPalindrome(String s) {
+        // проверка, что строка является палиндромом
+        return s.equals(reverse(s));
+    } 
+}
+    // обяект это экземпляр класса
+    // класс это шаблон для создания обьекта
